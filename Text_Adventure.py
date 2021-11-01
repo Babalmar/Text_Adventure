4965315a3b42191861175f14f9fe6f7a9109f76c
Babalmar <marcin.babalski@gmail.com>
2021-11-01 20:26:26 +0100

    Option selection + description text loaded from the file

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -5,3 +5,30 @@ equipment = ["Flask", "Rusty sword", "Pendant"]
 def equipement_list():
     print('Equipment:', ', '.join(equipment))
 
+def display_text(filename):
+    with open(filename, 'r') as f:
+        text = f.read()
+        print(text)
+        f.close()
+
+display_text('start.txt')
+equipement_list()
+while True:
+    decision = input("Which path will you choose?\n>>> ")
+    if decision.lower() not in ('n', 'e', 'w', 's'):
+        print("Not an appropriate choice.")
+    else:
+        break
+if decision == 'n':
+    os.system('cls')
+    print('You went to the forest')
+    equipement_list()
+elif decision == 'e':
+    print('You went to the village')
+    equipement_list()
+elif decision == 'w':
+    print('You went to the swamp')
+    equipement_list()
+elif decision == 's':
+    print('You went to the cementery')
+    equipement_list()
